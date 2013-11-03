364c77099a80df6e14c92e39a6e54489dea80998
Logan Walls <logan.w.gm@gmail.com>
2013-11-02 22:57:17 -0700

    Restructured the way task inputs are handled
    Now only one form, and one input page are used for all of the subtasks
    (including children's children, and so on.) When the user wants to add
    a new level of children, the current tasks are all cleared. (They are
    already stored in data representation in JS).

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,23 +3,26 @@
 $(document).ready(
 
 	function(){
-		$("#goal_name_next").click(newGoal);
+		$("#goal_name_next").bind("click", newGoal);
+		$("#difficulty").bind("change", updateDifficulty);
+		$("#add_subtask").bind("change", addSubtask);
+
 		window.goals = [];
 	}
 );
 
 function newGoal(){
 	var goalName = $("#goal_name").val();
-	$("#subtasks>div>h1").html(goalName);
-	window.currentGoal = new Goal(goalName);
-	window.goals.append(currentGoal);
-
-	addSubtask($("#subtask_form0"));
+	$("#subtask_page>div>h1").html(goalName);
+	window.currentGoal = new Goal(goalName, window.goals.length);
+	window.goals.push(currentGoal);
+	window.currentParent = currentGoal;
 }
 
+//label = the label corresopnding to the slider
 function updateDifficulty(){
 	var sliderValue = $(this).val();
-	var label = $("#difficulty_label");
+	var label = $('#difficulty_label');
 
 	switch(sliderValue){
 
@@ -40,27 +43,47 @@ function updateDifficulty(){
 	}
 }
 
-function Goal(name){
+function Goal(name, number, num){
 	this.goalName = name;
-	this.tasks = [];
+	this.pageform = this.pageform = $('subtask_form'+num);
+	this.subtasks = [];
+}
 
+function Task(name, difficulty){
+	this.taskName = name;
+	this.difficulty = difficulty;
 
-
-	//form = target form to add the subtask to.
-	function addSubtask(form){
-
-		form.append('<div id="task_div'+n+'"></div>');
-		var formContainer = $('#task_div'+n);
-
-		formContainer.append('<label for="goal_name">Task Name</label>');
-		formContainer.append('<input type="text" id="task_name'+n+'"/>');
-	    formContainer.append('<label id="difficulty_label" for="difficulty">Task Difficulty: None</label>');
-	    formContainer.append('<input id="difficulty'+n+'" class="ui-hidden-accessible" data-type="range" value="0" min="0" max="3" step="1" data-highlight="true">');
-	    $("#difficulty").bind("change", updateDifficulty);
+	this.render = render;
+	function render(){
+		$('subtasks').append("<div><h2>"+this.taskName+"</h2><h3>Difficulty: "+this.difficulty+"</h3></div>");
 	}
-
 }
 
-function Task(name, value){
-
+function addSubtask(){
+	var taskName = $('task_name').val();
+	var difficulty = $('difficulty').val();
+	var currentTask = new Task(taskName, difficulty);
+	currentParent.subtasks.push(currentTask);
+	currentTask.render();
 }
+
+function addChildren(newParent){
+	$('subtasks').html("");
+	$("#subtask_page>div>h1").html(newParent.taskName);
+	window.currentParent = newParent;
+	$.mobile.changePage("#subtask_page", allowSamePageTransition="true");
+
+	for(var i; i < newParent.subtasks.length; i++){
+		newParent.subtasks[i].render;
+	}
+}
+	// var n = window.currentGoal.subtask.length;
+	// var form = document.createElement("form");
+	// form.id = 
+	// form.append('<div id="task_div'+n+'"></div>');
+	// var formContainer = $('#task_div'+n);
+
+	// formContainer.append('<label for="task_name">Task Name</label>');
+	// formContainer.append('<input type="text" class="task_name"/>');
+ //    formContainer.append('<label class="difficulty_label" for="difficulty">Task Difficulty: None</label>');
+ //    formContainer.append('<input class="difficulty" class="ui-hidden-accessible" data-type="range" value="0" min="0" max="3" step="1" data-highlight="true">');
